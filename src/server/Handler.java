7ce6ec103fbce79bd1a52225096bda23a7356fe1
Joel Manning <stopwatchj@gmail.com>
2016-06-05 14:47:04 -0700

    Basic Server

---
+++ src/server/Handler.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package server;
+
+import java.net.Socket;
+
+/**
+ * @author joelmanning
+ *
+ */
+public class Handler extends Thread
+{
+    public Handler(Socket sock){
+        
+    }
+}
--- src/server/MegaServer.java
+++ src/server/MegaServer.java
@@ -3,20 +3,55 @@
  */
 package server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 /**
  * @author joelmanning
  *
  */
 public class MegaServer
 {
-
+    public static final int PORT = 599;
+    
+    private ServerDisplay display;
+    
+    private ServerSocket sock;
+    
     /**
      * @param args
      */
     public static void main(String[] args)
     {
-        // TODO Auto-generated method stub
-
+        MegaServer server = new MegaServer();
+    }
+    
+    public MegaServer(){
+        display = new SimpleServerDisplay();
+        display.printInfo("Server initializing.", 0);
+        display.printInfo("Creating socket on port " + PORT + ".", 0);
+        try
+        {
+            sock = new ServerSocket(PORT);
+        }
+        catch (IOException e)
+        {
+            display.printError("Error creating socket, shutting down.", 10);
+            System.exit(0);
+        }
+        display.printInfo("Created socket.", 0);
+        display.printInfo("Listening for connections.", 0);
+        while(true){
+            try
+            {
+                (new Handler(sock.accept())).start();
+                display.printInfo("Got a connection.", 0);
+            }
+            catch (IOException e)
+            {
+                display.printError("Error recieving a connection.", 5);
+            }
+        }
     }
 
 }
